bec0cbf8c51548fc405c13e7c5301b3772eb66ad
xelios34 <xelioselita@gmail.com>
2026-09-06 12:27:11 +0300

    Update car_repair.lua

--- car_repair.lua
+++ car_repair.lua
@@ -14,11 +14,7 @@ function script.update(dt)
 
     
 
-    -- F12 teleport only works when the car is stopped.
-    local car = ac.getCar(0)
-    local speedKmh = car and math.abs(car.speedKmh) or 0
-
-    if ac.isKeyDown(123) and counter >= 1 and speedKmh < 1 then
+    if ac.isKeyDown(123) and counter >= 1 then
         physics.setCarPosition(0,camposs,-campossdir)
         counter = 0
     end    
